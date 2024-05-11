4f46ddec8613452bab2a41c6a1256260a3c67a84
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-05-11 15:20:30 +0100

    Add score when blocks broken

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,19 +1,11 @@
-extends StaticBody2D
-
-signal block_broken
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.text = "0"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_area_2d_body_entered(body):
-	queue_free()
-	emit_signal("block_broken")
-
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var brick_scene = load("res://scenes/brick.tscn")
+var score: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,9 +9,14 @@ func _ready():
 		for j in 8:
 			var brick = brick_scene.instantiate()
 			brick.global_position = Vector2( 1280 / 32 + i*1280/16, 20 + j*40)
+			brick.block_broken.connect(_on_brick_broken)
 			add_child(brick)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_brick_broken():
+	score += 1
+	$HUD/Label.text = str(score)
